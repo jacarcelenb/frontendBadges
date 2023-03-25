f4d820ad89ed584940feb16c97dcbff82233825c
jacarcelenb <carcelenjorge17@gmail.com>
2023-03-25 17:17:41 -0500

    Colocar boton para ocultar y visualizar password

--- src/app/components/new-login/new-login.component.ts
+++ src/app/components/new-login/new-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertService } from 'src/app/services/alert.service';
@@ -13,37 +13,57 @@ export class NewLoginComponent implements OnInit {
   loginForm: FormGroup;
   datosCorrectos: boolean = true;
   textoError: string = '';
-  constructor(  private formBuilder: FormBuilder,
+
+  @ViewChild('passwordField2') passwordField2: ElementRef;
+
+  //passwordIcon
+  @ViewChild('passwordIcon2') passwordIcon2: ElementRef;
+  type: string = 'SHOW_PASSWORD';
+  type1: string = 'SHOW_PASSWORD';
+  constructor(private formBuilder: FormBuilder,
     private authService: AuthService,
     private router: Router,
     private alertService: AlertService) { }
 
-    ngOnInit(): void {
-      this.initForm();
-    }
-    initForm() {
-      this.loginForm = this.formBuilder.group({
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required]],
-      });
-    }
+  ngOnInit(): void {
+    this.initForm();
+  }
+  initForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
 
-    onSubmit() {
-      /**
-       * present loading while waiting response from api auth
-       */
+  onSubmit() {
+    /**
+     * present loading while waiting response from api auth
+     */
 
-      this.authService
-        .login(this.loginForm.value.email, this.loginForm.value.password)
-        .subscribe(
-          () => {
-            /**
-             * dismiss loading when operation is complete
-             */
-            this.initForm();
-            this.router.navigate(['experiment/step']);
-          }
-        );
+    this.authService
+      .login(this.loginForm.value.email, this.loginForm.value.password)
+      .subscribe(
+        () => {
+          /**
+           * dismiss loading when operation is complete
+           */
+          this.initForm();
+          this.router.navigate(['experiment/step']);
+        }
+      );
+  }
+
+  viewPasswordTwo() {
+    if (this.passwordField2.nativeElement.type == "password") {
+      this.passwordField2.nativeElement.type = "text";
+      this.passwordIcon2.nativeElement.className = 'fa fa-eye-slash';
+      this.type1 = 'HIDE_PASSWORD'
+    } else {
+      this.passwordField2.nativeElement.type = "password";
+      this.passwordIcon2.nativeElement.className = 'fa fa-eye';
+      this.type1 = 'SHOW_PASSWORD'
     }
+  }
+
 
 }
