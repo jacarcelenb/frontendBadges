3038119a135ecf8b09e3507349fd96c1a7f8442b
jacarcelenb <carcelenjorge17@gmail.com>
2023-04-22 17:40:07 -0500

    Prueba para el menu de guía de usuario

--- src/app/components/user-guide-stepper/user-guide-stepper.component.ts
+++ src/app/components/user-guide-stepper/user-guide-stepper.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-user-guide-stepper',
@@ -6,11 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-guide-stepper.component.scss']
 })
 export class UserGuideStepperComponent implements OnInit {
-change_language: any;
-
+  change_language: any;
+  currentStep: 0;
+ @ViewChild('stepper') stepper
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  click(){
+    console.log(this.currentStep)
+    console.log(this.stepper)
+    this.stepper._selectedIndex = 3 // restar dos
+    console.log(this.stepper._selectedIndex)
+    console.log(this.stepper)
+    this.stepper.next()
+
   }
 
 }
