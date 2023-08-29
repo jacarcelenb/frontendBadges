6dd05e0327f3a5175fb46b4badf4baa8307c3d05
jacarcelenb <carcelenjorge17@gmail.com>
2023-08-29 17:02:59 -0500

    Mejora de interfaz

--- src/app/components/upload-package/upload-package.component.ts
+++ src/app/components/upload-package/upload-package.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-upload-package',
@@ -9,7 +9,9 @@ export class UploadPackageComponent implements OnInit {
   isSelected: boolean = true;
   isTokenOption: boolean = true;
   NoPersonalToken: boolean = true;
-
+ @ViewChild('contentrepo') contentrepo :ElementRef;
+  showOptImage: boolean;
+  showOptPublication: boolean;
   constructor() { }
 
   ngOnInit(): void {
@@ -17,14 +19,26 @@ export class UploadPackageComponent implements OnInit {
 
   onChangeOption(checked: boolean) {
     this.isSelected = checked;
-  }
-
+  }ElementRefElementRefElementRef
   onCheckTokenOption(checked: boolean) {
     this.isTokenOption = checked;
   }
 
   onCheckPersonalToken(checked: boolean) {
     this.NoPersonalToken = checked;
+  }
+
+  verifyContent(){
+   if (this.contentrepo.nativeElement.value=='publication') {
+        this.showOptPublication = true;
+        this.showOptImage = false;
+   }else if(this.contentrepo.nativeElement.value=='image'){
+        this.showOptImage = true;
+        this.showOptPublication = false;
+   } else{
+     this.showOptImage = false;
+     this.showOptPublication = false;
+   }
   }
 
 }
