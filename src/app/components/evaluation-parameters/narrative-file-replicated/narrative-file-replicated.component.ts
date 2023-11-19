10a8294ccde1d2fb1da4c0a38a4831e27a02ecf9
jacarcelenb <carcelenjorge17@gmail.com>
2023-11-19 14:36:33 -0500

    narrative-file-replicated works

--- src/app/components/evaluation-parameters/narrative-file-replicated/narrative-file-replicated.component.ts
+++ src/app/components/evaluation-parameters/narrative-file-replicated/narrative-file-replicated.component.ts
@@ -59,7 +59,8 @@ export class NarrativeFileReplicatedComponent implements OnInit {
   id_artifact: any;
   change_language = false;
   artifact: any;
-
+  update_artifact: boolean = false;
+  @ViewChild('closeUpdateModal') closeUpdateModal: ElementRef;
   constructor(private actRoute: ActivatedRoute,
     private artifactController: ArtifactController,
     private alertService: AlertService,
@@ -553,11 +554,12 @@ export class NarrativeFileReplicatedComponent implements OnInit {
     this._artifactService.update(this.id_artifact,artifact).subscribe(() => {
       this.alertService.presentSuccessAlert(this.translateService.instant("MSG_UPDATE_ARTIFACT"));
       this.getUploadedArtifacts();
+      this.closeUpdateModal.nativeElement.click();
 
     });
   }
-  generatePDFfile() {
-    const doc = new jsPDF();
+  generatePDFfile(artifact) {
+    const doc = new jsPDF({ filters: ["ASCIIHexEncode"] });
     let date = new Date();
     let fecha = formatDate(date)
     if (this.textfile.nativeElement.value.trim().length == 0) {
@@ -820,7 +822,7 @@ export class NarrativeFileReplicatedComponent implements OnInit {
       let fileData = new File([blobPDF], "Replicated_Narrative_File.pdf", { type: blobPDF.type })
       this.file_format = blobPDF.type
       this.file_size = blobPDF.size
-      this.uploadGenerateArtifact(fileData)
+      this.uploadGenerateArtifact(fileData, artifact);
     }
 
   }
@@ -833,7 +835,7 @@ export class NarrativeFileReplicatedComponent implements OnInit {
     this.closeModal.nativeElement.click()
   }
 
-  uploadGenerateArtifact(file) {
+  uploadGenerateArtifact(file, artifact) {
     const artifact_name = parseArtifactNameForStorage(
       file.name,
     );
@@ -847,12 +849,28 @@ export class NarrativeFileReplicatedComponent implements OnInit {
       file,
       { onPercentageChanges },
       (storage_ref, file_url) => {
-        this.save(file_url, storage_ref, true);
-        this.createEvaluationStandard()
-        this.getEvaluationsBadges();
-        this.getValueEvaluation();
+        if (this.update_artifact) {
+          artifact.file_location_path = storage_ref
+          artifact.file_url = file_url
+          artifact.file_size = file.size
+          this.UpdateArtifacFile(artifact)
+        } else {
+          this.save(file_url, storage_ref, true);
+          this.createEvaluationStandard()
+          this.getEvaluationsBadges();
+          this.getValueEvaluation();
+        }
+
       },
     );
+  }
+
+  UpdateArtifacFile(artifact) {
+    this._artifactService.update(artifact._id, artifact).subscribe(() => {
+      this.getUploadedArtifacts();
+      this.alertService.presentSuccessAlert(this.translateService.instant('ARTIFACT_UPDATE_SUCCESS'))
+      this.closeModal.nativeElement.click();
+    })
   }
   getArtifact(artifact) {
     this.artifact = artifact;
@@ -865,10 +883,10 @@ export class NarrativeFileReplicatedComponent implements OnInit {
 
   GenerateNewFile() {
     if (this.artifact?._id.length > 0) {
-      this.deleteArtifact(this.artifact);
-      this.generatePDFfile();
+      this.update_artifact = true;
+      this.generatePDFfile(this.artifact);
     } else {
-      this.generatePDFfile();
+      this.generatePDFfile({});
     }
   }
 
