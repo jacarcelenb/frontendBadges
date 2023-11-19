40406ec31fa2c3ff4beb13346a73b2b49db0343f
jacarcelenb <carcelenjorge17@gmail.com>
2023-11-18 20:20:03 -0500

    justification-file-replicated works

--- src/app/components/evaluation-parameters/justification-file-replicated/justification-file-replicated.component.ts
+++ src/app/components/evaluation-parameters/justification-file-replicated/justification-file-replicated.component.ts
@@ -57,7 +57,8 @@ export class JustificationFileReplicatedComponent implements OnInit {
   id_artifact: any;
   change_language = false;
   artifact: any;
-
+  update_artifact: boolean = false;
+  @ViewChild('closeUpdateModal') closeUpdateModal: ElementRef;
   constructor(private actRoute: ActivatedRoute,
     private artifactController: ArtifactController,
     private alertService: AlertService,
@@ -68,9 +69,7 @@ export class JustificationFileReplicatedComponent implements OnInit {
     private _experimenterService: ExperimenterService,
     private translateService: TranslateService,
     private _artifactService: ArtifactService,
-    private fileSaverService: FileSaverService,
-    private _authService: AuthService,
-    private tokenStorageService: TokenStorageService,) { }
+    private fileSaverService: FileSaverService,) { }
 
   ngOnInit(): void {
     this.id_experiment = this.actRoute.parent.snapshot.paramMap.get('id');
@@ -554,11 +553,12 @@ export class JustificationFileReplicatedComponent implements OnInit {
     this._artifactService.update(this.id_artifact, artifact).subscribe(() => {
       this.alertService.presentSuccessAlert(this.translateService.instant("MSG_UPDATE_ARTIFACT"));
       this.getUploadedArtifacts();
+      this.closeUpdateModal.nativeElement.click();
 
     });
   }
-  generatePDFfile() {
-    const doc = new jsPDF();
+  generatePDFfile(artifact) {
+    const doc = new jsPDF({ filters: ["ASCIIHexEncode"] });
     let date = new Date();
     let fecha = formatDate(date)
 
@@ -824,7 +824,7 @@ export class JustificationFileReplicatedComponent implements OnInit {
       let fileData = new File([blobPDF], "Replicated_Justification_File.pdf", { type: blobPDF.type })
       this.file_format = blobPDF.type
       this.file_size = blobPDF.size
-      this.uploadGenerateArtifact(fileData)
+      this.uploadGenerateArtifact(fileData, artifact);
 
     }
 
@@ -838,7 +838,8 @@ export class JustificationFileReplicatedComponent implements OnInit {
     this.closeModal.nativeElement.click()
   }
 
-  uploadGenerateArtifact(file) {
+
+  uploadGenerateArtifact(file, artifact) {
     const artifact_name = parseArtifactNameForStorage(
       file.name,
     );
@@ -852,12 +853,28 @@ export class JustificationFileReplicatedComponent implements OnInit {
       file,
       { onPercentageChanges },
       (storage_ref, file_url) => {
-        this.save(file_url, storage_ref, true);
-        this.createEvaluationStandard()
-        this.getEvaluationsBadges();
-        this.getValueEvaluation();
+        if (this.update_artifact) {
+          artifact.file_location_path = storage_ref
+          artifact.file_url = file_url
+          artifact.file_size = file.size
+          this.UpdateArtifacFile(artifact)
+        } else {
+          this.save(file_url, storage_ref, true);
+          this.createEvaluationStandard()
+          this.getEvaluationsBadges();
+          this.getValueEvaluation();
+        }
+
       },
     );
+  }
+
+  UpdateArtifacFile(artifact) {
+    this._artifactService.update(artifact._id, artifact).subscribe(() => {
+      this.getUploadedArtifacts();
+      this.alertService.presentSuccessAlert(this.translateService.instant('ARTIFACT_UPDATE_SUCCESS'))
+      this.closeModal.nativeElement.click();
+    })
   }
   getArtifact(artifact) {
     this.artifact = artifact;
@@ -865,12 +882,11 @@ export class JustificationFileReplicatedComponent implements OnInit {
   }
   GenerateNewFile() {
     if (this.artifact?._id.length > 0) {
-      this.deleteArtifact(this.artifact);
-      this.generatePDFfile();
+      this.update_artifact = true;
+      this.generatePDFfile(this.artifact);
     } else {
-      this.generatePDFfile();
+      this.generatePDFfile({});
     }
   }
-
 
 }
