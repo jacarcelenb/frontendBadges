69150412815fe1c207171e704309e2af1b512318
jacarcelenb <carcelenjorge17@gmail.com>
2023-12-09 07:14:42 -0500

    cambio de url

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class EnvService {
 
-    // API_URL = 'https://backendbadgego.onrender.com/api/';
-  API_URL = 'http://localhost:4000/api/';
+    API_URL = 'https://backendbadgego.onrender.com/api/';
+   // API_URL = 'http://localhost:4000/api/';
   API_URL_NODE = this.API_URL;
   ASSETS_URL = ' /assets/';
   USER_TOKEN = 'user-token';
